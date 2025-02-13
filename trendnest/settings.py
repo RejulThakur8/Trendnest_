70da6b5ec5aef652d9eef2d922c7cf8d37d97f2e
RejulThakur8 <rejulthakur210@gmail.com>
2025-02-13 19:06:02 +0530

    Modify the urls

--- trendnest/settings.py
+++ trendnest/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-87kqh2+4lz74dg8)pp)km$pvak4_#g+o19s49bmg%%8@f*e18y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["trendnest.onrender.com", "localhost", "127.0.0.1"]
 
