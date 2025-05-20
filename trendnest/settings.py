7de3479225edc329cbbb6cafe0f20b95ad2ded78
RejulThakur8 <rejulthakur210@gmail.com>
2025-05-20 23:40:52 +0530

    Use environment variable for SECRET_KEY in settings.py

--- trendnest/settings.py
+++ trendnest/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87kqh2+4lz74dg8)pp)km$pvak4_#g+o19s49bmg%%8@f*e18y'
+# SECRET_KEY = 'django-insecure-87kqh2+4lz74dg8)pp)km$pvak4_#g+o19s49bmg%%8@f*e18y'
+
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-87kqh2+4lz74dg8)pp)km$pvak4_#g+o19s49bmg%%8@f*e18y")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -80,11 +83,11 @@ WSGI_APPLICATION = 'trendnest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST':  os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST':  os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
