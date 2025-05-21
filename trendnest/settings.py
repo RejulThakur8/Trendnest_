246267b5eb05da0833c8618f95c9bbdd43d561b4
RejulThakur8 <rejulthakur210@gmail.com>
2025-05-21 20:49:25 +0530

    Customize the urls

--- trendnest/settings.py
+++ trendnest/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-87kqh2+4lz74dg8)pp)km$pvak
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
